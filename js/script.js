ed8215c8d3fb134877dbbfe1eecaf1daed0b3297
Ashish Gupta <ashishgupta19@outlook.in>
2025-02-22 22:45:39 +0530

    Update script.js
    login form code to handle popup open and close behavior

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 };
+
+
+//Login form 
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.add('active');
+};
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.remove('active');
+};
